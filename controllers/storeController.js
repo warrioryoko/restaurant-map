51747238458d74f6b4e79a1fcb3fed4e88886d32
warrioryoko <miangpellegri@gmail.com>
2020-02-02 12:44:46 -0800

    Added new routes and controllers to handle updating store pages.

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -16,3 +16,30 @@ exports.createStore = async (req, res) => {
   req.flash('success', `Successfully Created ${store.name}. Care to leave a review?`);
   res.redirect(`store/${store.slug}`);
 }
+
+exports.getStores = async (req, res) => {
+  // 1. Query database for list of stores
+  const stores = await Store.find();
+  res.render('stores', {title: 'Stores', stores});
+}
+
+exports.editStore = async (req, res) => {
+  // 1. Find store, given the ID
+  const store = await Store.findOne({ _id: req.params.id });
+  // 2. Confirm the user is the owner of the store
+  // TODO
+  // 3. Render the edit form so users can update their store
+  res.render('editStore', { title: `Edit ${store.name}`, store });
+}
+
+exports.updateStore = async (req, res) => {
+  // 1. Find and update store
+  // const store = Store.findOneAndUpdate(q ,data, options);
+  const store = await Store.findOneAndUpdate({ _id: req.params.id }, req.body, { 
+    new: true, //return the new store instead of the old one
+    runValidators: true
+  }).exec();
+  // 2. Redirect user to the store, flash that it worked
+  req.flash('success', `Successfully updated <strong>${store.name}</strong>. <a href="/stores/${store.slug}">View Store -></a>`);
+  res.redirect(`/stores/${store._id}/edit`);
+}
